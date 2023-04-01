9f131bd8081c16d621ef657b139b46398ceaf520
Kelly Jiang <kelly.jiang@avanade.com>
2023-03-31 17:53:07 -0700

    In progress - merging CustomMarker and Marker

--- components/CustomMarker.js
+++ components/CustomMarker.js
@@ -1,38 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-// import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { db } from "../firebase";
 import {
-    getFirestore,
+
     collection,
     onSnapshot,
-    addDoc,
-    doc,
-    updateDoc,
-    arrayUnion,
+
 } from "firebase/firestore";
-import { pink } from 'color-name';
+import * as Location from 'expo-location';
+import LottieView from 'lottie-react-native'
+
 
 const CustomMarker = () => {
+    console.log("inside the function")
     const [region, setRegion] = useState(null);
     const [data, setData] = useState([])
-    const initialRegion = {
-        latitude: 49.2827291,
-        longitude: -123.1207375,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    };
-    const [selectedLocation, setSelectedLocation] = useState(null);
+
+    // const [selectedLocation, setSelectedLocation] = useState(null);
+    const [userLocation, setuserLocation] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [mapLayout, setMapLayout] = useState(null);
+
+    // ask for userpermision
+    useEffect(() => {
+        console.log("Requesting user permission...");
+
+        async function requestLocationPermission() {
+            const status = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied');
+                
+                return;
+            }
+
+            console.log("Fetching user location...");
+            const location = await Location.getCurrentPositionAsync({});
+            setuserLocation(location);
+            setIsLoading(false);
+        }
+
+        requestLocationPermission();
+    }, []);
 
 
 
 
+    console.log("line 26")
     // get location from firebase 
     useEffect(() => {
+        console.log("inside useffect")
         const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
             const data = snapshot.docs.map((doc) => {
                 const post = doc.data();
+                console.log("POST", post)
 
                 if ("location" in post && "address" in post) {
                     return {
@@ -52,20 +74,50 @@ const CustomMarker = () => {
                 }
             }).filter((post) => post !== null);
             setData(data);
-            // console.log("DATA ", data)
+            console.log("DATA ", data)
         });
         return () => unsubscribe();
     }, []);
 
 
+    // Get address from user location
+    console.log("line 59")
+
+
+    useEffect(() => {
+
+        const reverseGeocode = async () => {
+            console.log(" inside reverse geo")
+            if (userLocation) {
+                const [result] = await Location.reverseGeocodeAsync({
+                    latitude: userLocation.coords.latitude,
+                    longitude: userLocation.coords.longitude,
+                });
+
+                let formattedAddress = `${result.name}, ${result.street}, ${result.city}`;
+                setAddress(formattedAddress);
+            }
+        };
+
+        reverseGeocode();
+    }, [userLocation]);
 
 
 
-    // without geocodeing 
+
+
+
+    // Uses region to filter out displayed points from database
 
     const filterMarkers = () => {
-        console.log("original data:", data);
-        const { latitude: regionLat, longitude: regionLng, latitudeDelta, longitudeDelta } = region || initialRegion;
+        // console.log("original data:", data);
+        const { latitude: regionLat, longitude: regionLng, latitudeDelta, longitudeDelta } = region || {
+            latitude: 40.7128,
+            longitude: -74.006,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        };;
+        console.log("REGION ", region)
 
         const filteredLocations = data.map((marker) => marker.location)
             .filter((location) => {
@@ -84,7 +136,7 @@ const CustomMarker = () => {
         return filteredLocations;
     };
 
-    const handleRegionChangeComplete = (newRegion) => {
+    const handleRegionChange = (newRegion) => {
         setRegion(newRegion);
         // console.log('Map range:', newRegion);
     };
@@ -95,28 +147,74 @@ const CustomMarker = () => {
     const markersToShow = filterMarkers();
 
 
+
+    // Codeto fetch and store userLocation 
+    useEffect(() => {
+        const getUserLocation = async () => {
+            try {
+                console.log("Requesting user permission for location...")
+                const { status } = await Location.requestForegroundPermissionsAsync();
+
+                if (status !== 'granted') {
+                    console.log('Permission to access location was denied');
+                    return;
+                }
+
+                console.log("Getting user's current location...")
+                const location = await Location.getCurrentPositionAsync({});
+                console.log("USER LOCATION: ", location)
+                setuserLocation(location);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error while getting user location: ", error)
+                console.error("Error while getting user location")
+            }
+        }
+
+        getUserLocation();
+    }, []);
+
+    if (isLoading) {
+        return (
+
+            <View style={styles.container}>
+                <Text category={"h6"}>Loading</Text>
+                <LottieView
+                    autoPlay
+                    loop
+                    style={styles.lottie}
+                    source={require("../assets/Loading.json")}
+                />
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
 
-            <MapView
-                style={styles.map}
-                initialRegion={initialRegion}
-                onRegionChangeComplete={handleRegionChangeComplete}
-            >
-                {markersToShow.map((coordinate, index) => {
+            {userLocation ? (
+                <MapView
 
-                    return (
-                        <Marker
-                            key={`${coordinate.lat}-${coordinate.lng}-${index}`}
-                            coordinate={{ latitude: coordinate.lat, longitude: coordinate.lng }}
-                            title={coordinate.address}
-             
-                          
 
-                        />
-                    );
-                })}
-            </MapView>
+                    // add minHeight style
+                    style={{ height: '100' }}
+
+                    initialRegion={region || {
+                        latitude: userLocation.coords.latitude,
+                        longitude: userLocation.coords.longitude,
+                        latitudeDelta: 0.03,
+                        longitudeDelta: 0.03,
+                        // latitudeDelta: zoomLevel,
+                        // longitudeDelta: zoomLevel,
+                    }}
+
+                    onLayout={(event) => setMapLayout(event.nativeEvent.layout)}
+
+                    onRegionChangeComplete={handleRegionChange}
+                >
+
+                </MapView>) : (<View style={styles.mapPlaceholder} />)}
+
 
         </View>
     );
@@ -129,8 +227,38 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     map: {
-        ...StyleSheet.absoluteFillObject,
+        flex: 1,
     },
 });
 
 export default CustomMarker;
+
+
+
+{/* user Marker */ }
+{/* {mapLayout && userLocation && (
+
+                        <Marker
+                            coordinate={{
+                                latitude: userLocation.coords.latitude,
+                                longitude: userLocation.coords.longitude,
+                            }}
+                            title={address}
+                            pinColor={'blue'}
+                            opacity={0.85}
+                        />
+
+                    )} */}
+{/* {mapLayout && userLocation && markersToShow.map((coordinate, index) => {
+
+                        return (
+                            <Marker
+                                key={`${coordinate.lat}-${coordinate.lng}-${index}`}
+                                coordinate={{ latitude: coordinate.lat, longitude: coordinate.lng }}
+                                title={coordinate.address}
+
+
+
+                            />
+                        );
+                    })} */}
