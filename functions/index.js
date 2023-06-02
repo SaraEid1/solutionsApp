8226f237cef447266eee60500c75ed39985a1a16
Kelly <kellyjm2002@hotmail.com>
2023-06-02 13:06:44 -0700

    pre lock.json deletion to fix out of sync dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,22 @@
-/* eslint-disable */
-
-
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-admin.initializeApp();
 
+admin.initializeApp();
+const messaging = admin.messaging();
+console.log("ping recieved");
+
+// Exported functions for Firebase Emulator Suite testing
 exports.sendNotification = functions.firestore
   .document("posts/{postId}")
   .onCreate(async (snapshot) => {
     const post = snapshot.data();
-    const { title, body } = post;
+    const{ title, body }= post;
 
     // Get the FCM tokens of all users
     const tokensSnapshot = await admin.firestore().collection("users").get();
     const tokens = tokensSnapshot.docs.map((doc) => doc.data().fcmToken);
+    console.log("tokens: ", tokens)
 
     // Construct the notification payload
     const payload = {
@@ -25,5 +27,12 @@ exports.sendNotification = functions.firestore
     };
 
     // Send the notification to all users
-    await admin.messaging().sendToDevice(tokens, payload);
+    // await admin.messaging().sendToDevice(tokens, payload);
+
+    try {
+      const response = await messaging.sendEach(messages);
+      console.log("Notification sent successfully:", response);
+    } catch (error) {
+      console.error("Error sending notification:", error);
+    }
   });
