af5ee4e44aabfd245e4d387cf7660fe6ccb962e7
Kelly <kellyjm2002@hotmail.com>
2023-06-04 05:46:57 -0700

    hid receipients in email

--- functions/index.js
+++ functions/index.js
@@ -14,42 +14,47 @@ exports.sendNotification = functions.firestore
     const post = snapshot.data();
     const { title, body } = post;
 
-    let mailOptions = {
-      from: "notifications@EmpowerHer.com",
-      to: "", // Update with actual recipient(s) email address
-      subject: `New post - ${title}`,
-      text: `${body}:`,
-    };
-
     // Get the email addresses of all users
     const usersSnapshot = await db.collection("profiles").get();
     const emails = usersSnapshot.docs.map((doc) => doc.data().email);
 
-    if (emails.length > 0) {
-      mailOptions.to = emails.join(",");
-    } else {
-      console.log("No user emails found");
-      return;
-    }
+    const lowercaseBody = body.toLowerCase().replace(/\W/g, "");
+    const lowercaseTitle = title.toLowerCase().replace(/\W/g, "");
 
-    // Create a transport object using nodemailer with your email provider credentials
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "empowerHer.notifications@gmail.com",
-        pass: "sxeujsneadlpiuym",
-      },
+    const keywords = ["emergency", "urgent"];
+    const keywordExists = keywords.some((keyword) => {
+      // Check if any of the keywords exist in the post
+      const lowercaseKeyword = keyword.toLowerCase().replace(/\W/g, "");
+      return lowercaseBody.includes(lowercaseKeyword) || lowercaseTitle.includes(lowercaseKeyword);
     });
 
-    try {
-      // Send the email notification
-      await transporter.sendMail(mailOptions);
-      console.log("Notification email sent successfully");
-    } catch (error) {
-      console.error("Error sending notification email:", error);
+    if (keywordExists) {
+      let mailOptions = {
+        from: "EmpowerHer Notification",
+        to: "", // Leave this field empty to include recipients in BCC
+        bcc: emails.join(","),
+        subject: `New post - ${title}`,
+        text: `${body}`,
+      };
+
+      // Create a transport object using nodemailer with your email provider credentials
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "empowerHer.notifications@gmail.com",
+          pass: "sxeujsneadlpiuym",
+        },
+      });
+
+      try {
+        // Send the email notification
+        await transporter.sendMail(mailOptions);
+        console.log("Notification email sent successfully");
+      } catch (error) {
+        console.error("Error sending notification email:", error);
+      }
     }
   });
-
 // sneding notification using tokens to user device
 // exports.sendNotification = functions.firestore
 //   .document("posts/{postId}")
